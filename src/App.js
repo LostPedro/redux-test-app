84f85a8692c14fb8c29d9d9265fdbb5d6b4f3a4c
LostPedro <pepedro20@gmail.com>
2018-01-29 18:29:04 -0200

    Login Form

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBXbFPUNeZZ-deFBCWB3L6Ao6n5_JRkC4E',
+      authDomain: 'manager-f0818.firebaseapp.com',
+      databaseURL: 'https://manager-f0818.firebaseio.com',
+      projectId: 'manager-f0818',
+      storageBucket: '',
+      messagingSenderId: '317409183094'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
